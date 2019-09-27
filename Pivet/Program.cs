3f9c9706d2f4fd403487197beb675615855a3442
Tim Slater <tslater2006@gmail.com>
2019-09-27 10:38:53 -0500

    Add -e flag to make encrypting passwords easier for config file use

--- Pivet/Program.cs
+++ Pivet/Program.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security;
 
 namespace Pivet
 {    class Program
@@ -39,6 +41,7 @@ namespace Pivet
             var configFile = "config.json";
             var jobToRun = "";
             var wantsBuilder = false;
+            var passwordEncryptMode = false;
             ShowProgress = false;
 
             if (args.Length > 1)
@@ -79,6 +82,18 @@ namespace Pivet
                 {
                     ShowProgress = true;
                 }
+                if (args[0].ToLower().Equals("-e"))
+                {
+                    passwordEncryptMode = true;
+                }
+            }
+
+            if (passwordEncryptMode)
+            {
+                Console.Write("Enter the password you want to encrypt: ");
+                string pass = ReadPassword('*');
+                Console.WriteLine("Encrypted: " + PasswordCrypto.EncryptPassword(pass));
+                return;
             }
 
             if (File.Exists(configFile) == false)
@@ -155,6 +170,50 @@ namespace Pivet
             }
             Logger.Write("All done!");
 
+        }
+        public static string ReadPassword(char mask)
+        {
+            const int ENTER = 13, BACKSP = 8, CTRLBACKSP = 127;
+            int[] FILTERED = { 0, 27, 9, 10 /*, 32 space, if you care */ }; // const
+
+
+            SecureString securePass = new SecureString();
+
+            char chr = (char)0;
+
+            while ((chr = System.Console.ReadKey(true).KeyChar) != ENTER)
+            {
+                if (((chr == BACKSP) || (chr == CTRLBACKSP))
+                    && (securePass.Length > 0))
+                {
+                    System.Console.Write("\b \b");
+                    securePass.RemoveAt(securePass.Length - 1);
+
+                }
+                // Don't append * when length is 0 and backspace is selected
+                else if (((chr == BACKSP) || (chr == CTRLBACKSP)) && (securePass.Length == 0))
+                {
+                }
+
+                // Don't append when a filtered char is detected
+                else if (FILTERED.Count(x => chr == x) > 0)
+                {
+                }
+
+                // Append and write * mask
+                else
+                {
+                    securePass.AppendChar(chr);
+                    System.Console.Write(mask);
+                }
+            }
+
+            System.Console.WriteLine();
+            IntPtr ptr = new IntPtr();
+            ptr = Marshal.SecureStringToBSTR(securePass);
+            string plainPass = Marshal.PtrToStringBSTR(ptr);
+            Marshal.ZeroFreeBSTR(ptr);
+            return plainPass;
         }
     }
 
