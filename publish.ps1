e64051a58d552e043611cb0b35c075fe273d95c8
Timothy Slater <tslater2006@gmail.com>
2019-10-01 17:44:51 -0500

    Add globalization invariant setting. Add SelfContained builds.

--- publish.ps1
+++ publish.ps1
@@ -6,6 +6,10 @@ dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=Linux /p:Configuration=Rel
 dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=Windows /p:Configuration=Release -f netcoreapp2.2
 dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=RedHat /p:Configuration=Release -f netcoreapp2.2
 
+dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=LinuxSelfContained /p:Configuration=Release -f netcoreapp2.2
+dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=WindowsSelfContained /p:Configuration=Release -f netcoreapp2.2
+dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=RedHatSelfContained /p:Configuration=Release -f netcoreapp2.2
+
 If(!(test-path ".\build"))
 {
       mkdir build
@@ -19,3 +23,7 @@ Get-ChildItem -Recurse -Path .\Pivet\bin\Release\netcoreapp2.2\publish\ *.pdb | 
 Compress-Archive -Force -Path .\Pivet\bin\Release\netcoreapp2.2\publish\linux\* -DestinationPath ".\build\linux-$($hash).zip"
 Compress-Archive -Force -Path .\Pivet\bin\Release\netcoreapp2.2\publish\windows\* -DestinationPath ".\build\windows-$($hash).zip"
 Compress-Archive -Force -Path .\Pivet\bin\Release\netcoreapp2.2\publish\rhel\* -DestinationPath ".\build\redhat-$($hash).zip"
+
+Compress-Archive -Force -Path .\Pivet\bin\Release\netcoreapp2.2\publish\linux-self-contained\* -DestinationPath ".\build\linux-self-contained-$($hash).zip"
+Compress-Archive -Force -Path .\Pivet\bin\Release\netcoreapp2.2\publish\windows-self-contained\* -DestinationPath ".\build\windows-self-contained-$($hash).zip"
+Compress-Archive -Force -Path .\Pivet\bin\Release\netcoreapp2.2\publish\rhel-self-contained\* -DestinationPath ".\build\redhat-self-contained-$($hash).zip"
