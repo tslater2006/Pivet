66c3016f163a9d2433bb6262ea60716aff78a54c
Tim Slater <tslater2006@gmail.com>
2019-10-15 19:30:55 -0500

    Upgrade Pivet to .NET Core 3.0

--- publish.ps1
+++ publish.ps1
@@ -1,14 +1,14 @@
-if (Test-Path ".\Pivet\bin\Release\netcoreapp2.2\publish\") { 
-    Remove-Item ".\Pivet\bin\Release\netcoreapp2.2\publish\" -Recurse -Force; 
+if (Test-Path ".\Pivet\bin\Release\netcoreapp3.0\publish\") { 
+    Remove-Item ".\Pivet\bin\Release\netcoreapp3.0\publish\" -Recurse -Force; 
 }
 
-dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=Linux /p:Configuration=Release -f netcoreapp2.2
-dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=Windows /p:Configuration=Release -f netcoreapp2.2
-dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=RedHat /p:Configuration=Release -f netcoreapp2.2
+dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=Linux /p:Configuration=Release -f netcoreapp3.0
+dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=Windows /p:Configuration=Release -f netcoreapp3.0
+dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=RedHat /p:Configuration=Release -f netcoreapp3.0
 
-dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=LinuxSelfContained /p:Configuration=Release -f netcoreapp2.2
-dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=WindowsSelfContained /p:Configuration=Release -f netcoreapp2.2
-dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=RedHatSelfContained /p:Configuration=Release -f netcoreapp2.2
+dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=LinuxSelfContained /p:Configuration=Release -f netcoreapp3.0
+dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=WindowsSelfContained /p:Configuration=Release -f netcoreapp3.0
+dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=RedHatSelfContained /p:Configuration=Release -f netcoreapp3.0
 
 If(!(test-path ".\build"))
 {
@@ -18,12 +18,12 @@ If(!(test-path ".\build"))
 $commit= &git rev-parse HEAD 
 $hash = $commit.Substring(0,7)
 
-Get-ChildItem -Recurse -Path .\Pivet\bin\Release\netcoreapp2.2\publish\ *.pdb | foreach {Remove-Item -Path $_.FullName}
+Get-ChildItem -Recurse -Path .\Pivet\bin\Release\netcoreapp3.0\publish\ *.pdb | foreach {Remove-Item -Path $_.FullName}
 
-Compress-Archive -Force -Path .\Pivet\bin\Release\netcoreapp2.2\publish\linux\* -DestinationPath ".\build\linux-$($hash).zip"
-Compress-Archive -Force -Path .\Pivet\bin\Release\netcoreapp2.2\publish\windows\* -DestinationPath ".\build\windows-$($hash).zip"
-Compress-Archive -Force -Path .\Pivet\bin\Release\netcoreapp2.2\publish\rhel\* -DestinationPath ".\build\redhat-$($hash).zip"
+Compress-Archive -Force -Path .\Pivet\bin\Release\netcoreapp3.0\publish\linux\* -DestinationPath ".\build\linux-$($hash).zip"
+Compress-Archive -Force -Path .\Pivet\bin\Release\netcoreapp3.0\publish\windows\* -DestinationPath ".\build\windows-$($hash).zip"
+Compress-Archive -Force -Path .\Pivet\bin\Release\netcoreapp3.0\publish\rhel\* -DestinationPath ".\build\redhat-$($hash).zip"
 
-Compress-Archive -Force -Path .\Pivet\bin\Release\netcoreapp2.2\publish\linux-self-contained\* -DestinationPath ".\build\linux-self-contained-$($hash).zip"
-Compress-Archive -Force -Path .\Pivet\bin\Release\netcoreapp2.2\publish\windows-self-contained\* -DestinationPath ".\build\windows-self-contained-$($hash).zip"
-Compress-Archive -Force -Path .\Pivet\bin\Release\netcoreapp2.2\publish\rhel-self-contained\* -DestinationPath ".\build\redhat-self-contained-$($hash).zip"
+Compress-Archive -Force -Path .\Pivet\bin\Release\netcoreapp3.0\publish\linux-self-contained\* -DestinationPath ".\build\linux-self-contained-$($hash).zip"
+Compress-Archive -Force -Path .\Pivet\bin\Release\netcoreapp3.0\publish\windows-self-contained\* -DestinationPath ".\build\windows-self-contained-$($hash).zip"
+Compress-Archive -Force -Path .\Pivet\bin\Release\netcoreapp3.0\publish\rhel-self-contained\* -DestinationPath ".\build\redhat-self-contained-$($hash).zip"
