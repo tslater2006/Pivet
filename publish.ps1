f548f4d8a3f7a866d04b056c147e2d9d145857a5
tslater2006 <tslater2006@gmail.com>
2022-02-24 08:10:04 -0600

    Upgrade to .NET 6.0

--- publish.ps1
+++ publish.ps1
@@ -1,14 +1,6 @@
-if (Test-Path ".\Pivet\bin\Release\netcoreapp2.2\publish\") { 
-    Remove-Item ".\Pivet\bin\Release\netcoreapp2.2\publish\" -Recurse -Force; 
+if (Test-Path ".\Pivet\bin\Release\net6.0\publish\") { 
+    Remove-Item ".\Pivet\bin\Release\net6.0\" -Recurse -Force; 
 }
-
-dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=Linux /p:Configuration=Release -f netcoreapp2.2
-dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=Windows /p:Configuration=Release -f netcoreapp2.2
-dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=RedHat /p:Configuration=Release -f netcoreapp2.2
-
-dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=LinuxSelfContained /p:Configuration=Release -f netcoreapp2.2
-dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=WindowsSelfContained /p:Configuration=Release -f netcoreapp2.2
-dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=RedHatSelfContained /p:Configuration=Release -f netcoreapp2.2
 
 If(!(test-path ".\build"))
 {
@@ -18,10 +10,20 @@ If(!(test-path ".\build"))
 $commit= &git rev-parse HEAD 
 $hash = $commit.Substring(0,7)
 
-Compress-Archive -Force -Path .\Pivet\bin\Release\netcoreapp2.2\publish\linux\* -DestinationPath ".\build\linux-$($hash).zip"
-Compress-Archive -Force -Path .\Pivet\bin\Release\netcoreapp2.2\publish\windows\* -DestinationPath ".\build\windows-$($hash).zip"
-Compress-Archive -Force -Path .\Pivet\bin\Release\netcoreapp2.2\publish\rhel\* -DestinationPath ".\build\redhat-$($hash).zip"
+dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=Linux /p:Configuration=Release -f net6.0
+Compress-Archive -Force -Path .\Pivet\bin\Release\net6.0\linux-x64\* -DestinationPath ".\build\linux-$($hash).zip"
 
-Compress-Archive -Force -Path .\Pivet\bin\Release\netcoreapp2.2\publish\linux-self-contained\* -DestinationPath ".\build\linux-self-contained-$($hash).zip"
-Compress-Archive -Force -Path .\Pivet\bin\Release\netcoreapp2.2\publish\windows-self-contained\* -DestinationPath ".\build\windows-self-contained-$($hash).zip"
-Compress-Archive -Force -Path .\Pivet\bin\Release\netcoreapp2.2\publish\rhel-self-contained\* -DestinationPath ".\build\redhat-self-contained-$($hash).zip"
+dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=Windows /p:Configuration=Release -f net6.0
+Compress-Archive -Force -Path .\Pivet\bin\Release\net6.0\win10-x64\* -DestinationPath ".\build\windows-$($hash).zip"
+
+dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=RedHat /p:Configuration=Release -f net6.0
+Compress-Archive -Force -Path .\Pivet\bin\Release\net6.0\rhel-x64\* -DestinationPath ".\build\redhat-$($hash).zip"
+
+dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=LinuxSelfContained /p:Configuration=Release -f net6.0
+Compress-Archive -Force -Path .\Pivet\bin\Release\net6.0\linux-x64\* -DestinationPath ".\build\linux-self-contained-$($hash).zip"
+
+dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=WindowsSelfContained /p:Configuration=Release -f net6.0
+Compress-Archive -Force -Path .\Pivet\bin\Release\net6.0\win10-x64\* -DestinationPath ".\build\windows-self-contained-$($hash).zip"
+
+dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=RedHatSelfContained /p:Configuration=Release -f net6.0
+Compress-Archive -Force -Path .\Pivet\bin\Release\net6.0\rhel-x64\* -DestinationPath ".\build\redhat-self-contained-$($hash).zip"
