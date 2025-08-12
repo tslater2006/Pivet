5afaf3aa3acba372a09b7895d1ac2f902177592e
tslater2006 <tslater2006@gmail.com>
2025-08-12 13:30:09 -0500

    Remove RHEL support and update build system
    - Remove RedHat and RedHatSelfContained publish profiles
    - Update remaining publish profiles for .NET 8.0 framework
    - Update RuntimeIdentifier from win10-x64 to win-x64 in Windows profiles
    - Update PowerShell build script:
      - Change .NET 6.0 paths to .NET 8.0
      - Remove RHEL build targets
      - Update archive paths to use correct runtime identifiers

--- publish.ps1
+++ publish.ps1
@@ -1,5 +1,5 @@
-if (Test-Path ".\Pivet\bin\Release\net6.0\publish\") { 
-    Remove-Item ".\Pivet\bin\Release\net6.0\" -Recurse -Force; 
+if (Test-Path ".\Pivet\bin\Release\net8.0\publish\") { 
+    Remove-Item ".\Pivet\bin\Release\net8.0\" -Recurse -Force; 
 }
 
 If(!(test-path ".\build"))
@@ -10,20 +10,14 @@ If(!(test-path ".\build"))
 $commit= &git rev-parse HEAD 
 $hash = $commit.Substring(0,7)
 
-dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=Linux /p:Configuration=Release -f net6.0
-Compress-Archive -Force -Path .\Pivet\bin\Release\net6.0\linux-x64\* -DestinationPath ".\build\linux-$($hash).zip"
+dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=Linux /p:Configuration=Release -f net8.0
+Compress-Archive -Force -Path .\Pivet\bin\Release\net8.0\linux-x64\* -DestinationPath ".\build\linux-$($hash).zip"
 
-dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=Windows /p:Configuration=Release -f net6.0
-Compress-Archive -Force -Path .\Pivet\bin\Release\net6.0\win10-x64\* -DestinationPath ".\build\windows-$($hash).zip"
+dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=Windows /p:Configuration=Release -f net8.0
+Compress-Archive -Force -Path .\Pivet\bin\Release\net8.0\win-x64\* -DestinationPath ".\build\windows-$($hash).zip"
 
-dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=RedHat /p:Configuration=Release -f net6.0
-Compress-Archive -Force -Path .\Pivet\bin\Release\net6.0\rhel-x64\* -DestinationPath ".\build\redhat-$($hash).zip"
+dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=LinuxSelfContained /p:Configuration=Release -f net8.0
+Compress-Archive -Force -Path .\Pivet\bin\Release\net8.0\linux-x64\* -DestinationPath ".\build\linux-self-contained-$($hash).zip"
 
-dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=LinuxSelfContained /p:Configuration=Release -f net6.0
-Compress-Archive -Force -Path .\Pivet\bin\Release\net6.0\linux-x64\* -DestinationPath ".\build\linux-self-contained-$($hash).zip"
-
-dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=WindowsSelfContained /p:Configuration=Release -f net6.0
-Compress-Archive -Force -Path .\Pivet\bin\Release\net6.0\win10-x64\* -DestinationPath ".\build\windows-self-contained-$($hash).zip"
-
-dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=RedHatSelfContained /p:Configuration=Release -f net6.0
-Compress-Archive -Force -Path .\Pivet\bin\Release\net6.0\rhel-x64\* -DestinationPath ".\build\redhat-self-contained-$($hash).zip"
+dotnet publish .\Pivet\Pivet.csproj /p:PublishProfile=WindowsSelfContained /p:Configuration=Release -f net8.0
+Compress-Archive -Force -Path .\Pivet\bin\Release\net8.0\win-x64\* -DestinationPath ".\build\windows-self-contained-$($hash).zip"
